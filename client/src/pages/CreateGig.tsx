b48717eabb02cc745aaf5e11fb8a7441b22e36cc
Aniruddh <aniruddh.languify@gmail.com>
2026-01-14 17:48:11 +0530

    fix:fixed build issue in prod

--- client/src/pages/CreateGig.tsx
+++ client/src/pages/CreateGig.tsx
@@ -1,4 +1,4 @@
-import { useState, FormEvent } from 'react';
+import { useState, type FormEvent } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useAppDispatch } from '../hooks/useAppDispatch';
 import { createGig } from '../store/gigSlice';
